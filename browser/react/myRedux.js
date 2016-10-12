d986c577043888613ee12261e7cac7fc3eba19f6
rachael <hobbs.rachael@gmail.com>
2016-10-12 17:11:28 -0400

    before part 5 review

--- browser/react/myRedux.js
+++ browser/react/myRedux.js
@@ -3,11 +3,15 @@ import {applyMiddleware} from 'redux';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import {convertAlbum} from './containers/AppContainer';
+import {combineReducers} from 'redux';
 
 const GET_ALBUMS_FROM_SERVER = 'GET_ALBUMS_FROM_SERVER';
+const START_PLAYING = 'START_PLAYING';
+const STOP_PLAYING = 'STOP_PLAYING'
+const SET_CURRENT_SONG = 'SET_CURRENT_SONG'
 const initialState = require('./initialState.js');
 
-function reducer (state = initialState, action){
+function albums (state = initialState, action){
   switch (action.type){
     case GET_ALBUMS_FROM_SERVER:
       return Object.assign({}, state, {
@@ -32,5 +36,69 @@ export const fetchAlbumsFromServer =() => {
         dispatch(getAlbums(albums));
       })
 }
+export const play = () => dispatch => {
+  AUDIO.play();
+  dispatch(startPlaying());
+};
 
-export let store = redux.createStore(reducer, applyMiddleware(createLogger(), thunkMiddleware));
+export const pause = () => dispatch => {
+  AUDIO.pause();
+  dispatch(stopPlaying());
+};
+
+export const load = (currentSong, currentSongList) => dispatch => {
+  AUDIO.src = currentSong.audioUrl;
+  AUDIO.load();
+  dispatch(setCurrentSong(currentSong, currentSongList));
+};
+
+export const startSong = (song, list) => dispatch => {
+  dispatch(pause());
+  dispatch(load(song, list));
+  dispatch(play());
+};
+
+export const toggle = () => (dispatch, getState) => {
+  const { isPlaying } = getState();
+  if (isPlaying) dispatch(pause());
+  else dispatch(play());
+};
+
+export const toggleOne = (selectedSong, selectedSongList) =>
+  (dispatch, getState) => {
+    const { currentSong } = getState();
+    if (selectedSong.id !== currentSong.id)
+      dispatch(startSong(selectedSong, selectedSongList));
+    else dispatch(toggle());
+};
+
+function isPlaying (state = false, action){
+  switch (action.type) {
+    case START_PLAYING: return true;
+    case STOP_PLAYING: return false;
+    default: return state;
+  }
+}
+
+const currentSong = (state = {}, action) => {
+  switch (action.type) {
+    case SET_CURRENT_SONG: return action.currentSong;
+    default: return state;
+  }
+};
+
+const currentSongList = (state = [], action) => {
+  switch (action.type) {
+    case SET_CURRENT_SONG: return action.currentSongList;
+    default: return state;
+  }
+};
+
+export const rootReducer = combineReducers({
+  albums,
+  isPlaying,
+  currentSong,
+  currentSongList
+});
+
+export let store = redux.createStore(albums, applyMiddleware(createLogger(), thunkMiddleware));
